735bf12686186f0cbe5a0555b202182898d13ec3
shuvishma <36675563+shuvishma@users.noreply.github.com>
2021-08-19 02:07:09 +0530

    saved

--- src/components/App.js
+++ src/components/App.js
@@ -1,100 +1,125 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./../styles/App.css";
 
 function ListItem(props) {
-	const { children, onModify, i, onDelete } = props ;
-	const [isEditMode, setIsEditMode] = useState(false) ;
-	const [tempTask, setTempTask] = useState(children) ;
+  const { children, onModify, onDelete } = props;
 
-	return isEditMode ? (
-		<li>
-			<textarea 
-				className="editTask"
-				value={tempTask}
-				onChange={function (event) {
-					setTempTask(event.target.value) ;
-				}}></textarea>
-			<button
-				className="saveTask"
-				onClick={function () {
-					if(tempTask != '') {
-						onModify(tempTask, i) ;
-						setIsEditMode(false) ;
-					}
-				}}
-			>Save</button>
-		</li>
-	) : (
-		<>
-			<li className="list">{children}</li>
-			<button
-				onClick = {function () {
-					setIsEditMode(true) ;
-				}}
-			>Edit</button>
-			<button onClick={onDelete}>Remove</button>
-		</>	
-	) ;
+  const [isEditMode, setIsEditMode] = useState(false);
+  console.log({ isEditMode });
+  const [tempTask, setTempTask] = useState(children);
+
+  return isEditMode ? (
+    <>
+      <textarea
+        className="editTask"
+        onChange={function (event) {
+          setTempTask(event.target.value);
+        }}
+        value={tempTask}
+      ></textarea>
+      <button
+        className="saveTask"
+        type="button"
+        disabled={tempTask === ""}
+        onClick={function () {
+          console.log("save click", tempTask);
+          if (tempTask !== "") {
+            onModify(tempTask);
+            setIsEditMode(false);
+          }
+        }}
+      >
+        Save
+      </button>
+    </>
+  ) : (
+    <>
+      <li className="list" key={children}>
+        {children}
+      </li>
+      <button
+        className="edit"
+        type="button"
+        onClick={function () {
+          setIsEditMode(true);
+        }}
+      >
+        Edit
+      </button>
+      <button
+        className="delete"
+        type="button"
+        onClick={function () {
+          onDelete();
+        }}
+      >
+        Delete
+      </button>
+    </>
+  );
 }
 
-function App() 
-{
-	const [task, setTask] = useState('') ;
-	const [todoList, setTodoList] = useState([]) ;
+function App() {
+  const [task, setTask] = useState("");
+  const [todoList, setTodoList] = useState([]);
 
-	function onModify(updatedTask, index){
-		const newList = todoList.map(function (task, i) {
-			if(i == index) {
-				return updatedTask ;
-			}
-			return task ;
-		}) ;
-		setTodoList(newList) ;
-	}
+  function onModify(givenTask, givenI) {
+    console.log({ givenTask, givenI });
+    const newList = todoList.map(function (task, i) {
+      if (givenI === i) {
+        return givenTask;
+      }
+      return task;
+    });
+    // console.log(newList === todoList);
+    setTodoList(newList);
+  }
 
-	function onDelete(index) {
-		const newList = todoList.filter(function (task, i) {
-			return i !== index ;
-		}) ;
-		setTodoList(newList) ;
-	}
+  function onDelete(givenI) {
+    const newList = todoList.filter(function (el, i) {
+      return i !== givenI;
+    });
+    setTodoList(newList);
+  }
 
-	return (
-	<div id="main">
-		<textarea 
-			id="task" 
-			value={task} 
-			onChange={function(event) {
-				setTask(event.target.value) ;
-			}}>
-		</textarea>
+  return (
+    <div id="main">
+      <textarea
+        id="task"
+        onChange={function (event) {
+          setTask(event.target.value);
+        }}
+        value={task}
+      ></textarea>
+      <button
+        id="btn"
+        type="button"
+        onClick={function () {
+          if (task !== "") {
+            setTodoList([...todoList, task]);
+            setTask("");
+          }
+        }}
+      >
+        Add
+      </button>
 
-		<button 
-			id="btn"
-			onClick={function(){
-				if(task != '') {
-					setTodoList([...todoList, task]) ;
-					setTask('') ;
-				}
-			}}>Add task</button>
-
-		<ul>
-			{
-				todoList.map((task, i) => {
-					return <ListItem 
-						i={i}
-						onModify={onModify} 
-						onDelete={function () {
-							onDelete(i) ;
-						}}
-						key={task}
-					>{task}</ListItem>;
-				})
-			}
-		</ul>
-	</div>
-	);
+      <ul>
+        {todoList.map((task, i) => {
+          return (
+            <ListItem
+              onModify={(newTask) => {
+                onModify(newTask, i);
+              }}
+              onDelete={() => onDelete(i)}
+            >
+              {task}
+            </ListItem>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
-
 
 export default App;
